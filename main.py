287c408dfc87c66d4e0e1127de4b5813ab8f3002
Алексей <Lexa3896@mail.ru>
2023-02-05 22:43:45 +0300

    game logic

--- main.py
+++ main.py
@@ -157,3 +157,193 @@ class Button:
                             self.to_menu_button_action()
                         elif self.type == 'game':
                             self.game_button_action()
+
+
+
+
+def new_level(): # function of changing a level
+    global score, level_text_surface, personal_best_text_surface, new_level_flag, level_k
+    level_k = level_k / 1.1
+    screen.blit(background, (0, 0))
+    level_text_surface = pygame.font.SysFont("Calibri", 40).render('Level ' + str(score // 10 + 1), True, (0, 0, 0, 0))
+    new_level_show()
+    screen.blit(level_text_surface, (150 - level_text_surface.get_size()[0] // 2 - 1, 150 - level_text_surface.get_size()[1] // 2))
+    pygame.display.update()
+    new_level_show()
+    for _ in range(24):
+        pygame.display.update()
+        level_text_surface.set_alpha(_ * 10)
+        new_level_show()
+        pygame.display.update()
+        time.sleep(0.03125)
+    time.sleep(0.75)
+    for _ in range(24):
+        pygame.display.update()
+        level_text_surface.set_alpha((12 - _) * 10)
+        new_level_show()
+        pygame.display.update()
+        time.sleep(0.03125)
+
+    new_level_flag = False
+
+def answ(color): # when user is answering by pressing a button
+    global score, stop_flag, ans_flag, new_level_flag, score_text_surface, level_k
+    present_color = ShowColor.color
+    if color == present_color and not ans_flag:
+        score += 1
+        score_update()
+    elif not ans_flag:
+        score = 0
+        level_k = 1
+        score_update()
+    if score != 0 and score % 10 == 0:
+        new_level_flag = True
+    ans_flag = True
+    stop_flag = True
+
+
+def starttimer():
+    global stop_flag, ans_flag, menu_flag
+    if menu_flag:
+        exit()
+    prev_cl = ShowColor.color
+    t = threading.Thread(target=lambda: ShowColor.change_color(random.choice([_ for _ in ['red', 'blue', 'purple', 'yellow', 'green', (0, 255, 255)] if _ != prev_cl])))
+    t.start()
+    timer1sec = threading.Thread(target=wait1sec)
+    stop_flag = False
+    ans_flag = False
+    timer1sec.start()
+
+
+def wait1sec():
+    global stop_flag, new_level_flag, level_k
+    if stop_flag:
+        sys.exit(0)
+    time.sleep(level_k)
+    if mainloop_stop_flag or menu_flag:
+        exit(0)
+    elif new_level_flag:
+        new_level()
+    starttimer()
+    sys.exit(0)
+
+
+def mainloop():
+    global mainloop_stop_flag, menu_flag
+    if not menu_flag:
+        starttimer()
+    while True:
+        clock.tick(144)
+        screen.blit(background, (0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                mainloop_stop_flag = True
+                with open('data.json', 'w') as file:
+                    data['Personal Best'] = personal_best
+                    json.dump(data, file)
+                pygame.quit()
+                exit(0)
+            if not menu_flag:
+                all_button_click(event)
+            else:
+                MenuStartButton.click(event)
+        if menu_flag:
+            screen.blit(menu_personal_best_surface, menu_personal_best_surface.get_rect(center=(300/2, 150)))
+            screen.blit(menu_logo_surface, (50, 50))
+            MenuStartButton.show()
+            pygame.display.flip()
+        if not new_level_flag and not menu_flag:
+            game_show()
+            pygame.display.flip()
+
+
+def all_button_click(event):
+    RedButton.click(event)
+    GreenButton.click(event)
+    PurpleButton.click(event)
+    LightBlueButton.click(event)
+    YellowButton.click(event)
+    BlueButton.click(event)
+    GameButtonToMenu.click(event)
+
+
+def game_show():
+    screen.blit(background, (0, 0))
+    screen.blit(score_text_surface, (150 - score_text_surface.get_size()[0] // 2 - 1, 30))
+    screen.blit(personal_best_text_surface, (0, 0))
+    GameButtonToMenu.show()
+    RedButton.show()
+    GreenButton.show()
+    PurpleButton.show()
+    LightBlueButton.show()
+    YellowButton.show()
+    BlueButton.show()
+    ShowColor.show()
+
+
+def new_level_show():
+    screen.blit(background, (0, 0))
+    screen.blit(level_text_surface, (150 - level_text_surface.get_size()[0] // 2 - 1, 200 - level_text_surface.get_size()[1] // 2))
+    screen.blit(personal_best_text_surface, (0, 0))
+
+# interface blocks(buttons, rects, etc.)
+
+MenuStartButton = Button(
+    (50, 200),
+    (200, 50),
+    (0, 255, 255),
+    type='menu',
+    alpha=0,
+    image='media/TCFinalButton.png'
+)
+GameButtonToMenu = Button(
+    (250, 0),
+    (50, 20),
+    (0, 0, 0),
+    type='to_menu',
+    text='menu',
+    text_color='purple',
+    alpha=0
+)
+RedButton = Button(
+    (0, 300),
+    (100, 100),
+    'red',
+    type='game'
+)
+GreenButton = Button(
+    (0, 400),
+    (100, 100),
+    'green',
+    type='game'
+)
+PurpleButton = Button(
+    (100, 300),
+    (100, 100),
+    'purple',
+    type='game'
+)
+LightBlueButton = Button(
+    (100, 400),
+    (100, 100),
+    (0, 255, 255),
+    type='game'
+)
+YellowButton = Button(
+    (200, 300),
+    (100, 100),
+    'yellow',
+    type='game'
+)
+BlueButton = Button(
+    (200, 400),
+    (100, 100),
+    'blue',
+    type='game'
+)
+ShowColor = SimpleRect(
+    (100, 100),
+    (100, 100),
+    'blue',
+)
+mainloop()
